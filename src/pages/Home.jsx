cd3b8035ae6afc79b4b20800bb073211c36195df
Lurimo8 <lurimo2000@gmail.com>
2025-04-24 10:53:48 +0200

    Movies-App

---
+++ src/pages/Home.jsx
@@ -0,0 +1,105 @@
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
+import "../css/Home.css";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [hasSearched, setHasSearched] = useState(false);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+
+    setLoading(true);
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setHasSearched(true);
+      setError(null);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to search movies...");
+    } finally {
+      setLoading(false);
+    }
+
+    setSearchQuery("");
+  };
+
+  const handleBack = async () => {
+    if (loading) return;
+  
+    setLoading(true);
+    try {
+      const popularMovies = await getPopularMovies();
+      setMovies(popularMovies);
+      setHasSearched(false);
+      setError(null);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to load popular movies...");
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        {hasSearched && !searchQuery.trim() ? (
+          <button type="button" className="search-button" onClick={handleBack}>
+            Reset
+          </button>
+        ) : (
+          <button type="submit" className="search-button">
+            Search
+          </button>
+        )}
+      </form>
+
+      {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
